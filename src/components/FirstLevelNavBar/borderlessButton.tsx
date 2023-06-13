b43e03bd30c5d5a45346bd64347e31114c925209
liyuxyahhh <Yuxuan.Li17@alumni.xjtlu.edu.cn>
2023-06-13 13:29:08 -0400

    Change the name and position of the borderless button and update the file according to the instruction of Dylan

---
+++ src/components/FirstLevelNavBar/borderlessButton.tsx
@@ -0,0 +1,45 @@
+import { css } from "@emotion/css";
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+const BorderlessButton = ({
+  onClick,
+  children,
+}: {
+  onClick?: () => void,
+  children: string,
+}) => {
+  return (
+    <button
+      className={css`
+        width: 120px;
+        height: 24px;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #18181b;
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+      `}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export { BorderlessButton };
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
