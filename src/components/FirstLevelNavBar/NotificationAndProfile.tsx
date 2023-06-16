c1c9a0da55d6b05a3ff00e2fe9926b29d2973e31
liyuxyahhh <Yuxuan.Li17@alumni.xjtlu.edu.cn>
2023-06-16 15:23:47 -0400

    feat: NotificationAndProfile.tsx and first level Navbar and chore: delete useless folder and move graph into resoruces/img folder and add new images for notificaiton and useravatar

---
+++ src/components/FirstLevelNavBar/NotificationAndProfile.tsx
@@ -0,0 +1,36 @@
+import { css } from "@emotion/css";
+import Notification from "resources/img/Notification.svg";
+import UserAvatar from "resources/img/UserAvatar.svg";
+
+const NotificationAndProfile = () => {
+    return (
+        <div
+            className={css`
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: flex-end;
+                gap: 33px;
+                padding: 4px 0;
+                position: absolute;
+                right: 15px;
+                top: 16px;
+            `}
+        >
+            <img src={Notification} alt="Notification Icon" 
+                className={css`
+                    width: 32px;
+                    height: 32px;
+                `}
+            />
+            <img src={UserAvatar} alt="User Icon" 
+                className={css`
+                    width: 32px;
+                    height: 32px;
+                `}
+            />
+        </div>
+    );
+};
+
+export { NotificationAndProfile };
