dfe03f4880ea56372252b9b01141fa0237098206
liyuxyahhh <Yuxuan.Li17@alumni.xjtlu.edu.cn>
2023-06-16 22:11:21 -0400

    chore move my code for FirstLevelNavBar.tsx into index.tsx

--- src/components/FirstLevelNavBar/index.tsx
+++ src/components/FirstLevelNavBar/index.tsx
@@ -1,1 +1,33 @@
-export { FirstLevelNavBar } from "./FirstLevelNavBar";
+import { css } from "@emotion/css";
+import { LeftIcon } from "./LeftIcon";
+import { MiddlebuttonSection } from "./MiddlebuttonSection";
+import { NotificationAndProfile } from "./NotificationAndProfile";
+
+const FirstLevelNavBar = () => {
+    return (
+        <div
+            className={css`
+                box-sizing: border-box;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                width: 1440px;
+                height: 64px;
+                padding: 0 24px;
+                background: #FFFFFF;
+                border-bottom: 1px solid #E9E9EC;
+            `}
+        >
+            <LeftIcon />
+            <MiddlebuttonSection />
+            <div
+                className={css`
+                    width: 285.5px;
+                `}
+            />
+            <NotificationAndProfile />
+        </div>
+    );
+};
+
+export { FirstLevelNavBar };
