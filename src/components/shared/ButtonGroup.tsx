f96fadb21c5dd18b200957a0f9dc6257f6f8cb79
yutinggh <134508301+yutinggh@users.noreply.github.com>
2023-06-30 14:04:04 +0800

    Update ButtonGroup.tsx

--- src/components/shared/ButtonGroup.tsx
+++ src/components/shared/ButtonGroup.tsx
@@ -1,90 +1,59 @@
 import { css } from "@emotion/css";
-import { basis, primary, white } from "components/constants/colors";
 import React, { useState } from "react";
+import { basis, fill, primary } from "components/constants/colors";
 
-const ButtonGroup = () => {
-  const [isClicked, setIsClicked] = useState(false);
-  const handleClick = () => {
-    setIsClicked(!isClicked);
+interface ButtonGroupProps {
+  children: React.ReactNode;
+}
+function ButtonGroup(props: ButtonGroupProps) {
+  const [activeButton, setActiveButton] = useState<number | null>(null);
+  const [prevActiveButton, setPrevActiveButton] = useState<number | null>(null);
+  const handleButtonClick = (index: number) => {
+    setPrevActiveButton(activeButton);
+    setActiveButton(index);
   };
+  const childrenCount = React.Children.count(props.children);
   return (
     <div
       className={css`
-      box-sizing: border-box;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      align-self: stretch;
-      border-radius: 6px;
-      width: 208px;
-      height: 28px;
-      border-color: ${basis.border};
-      overflow: hidden;
-      background: ${isClicked ? basis.bg_subtle : white};
-      box-shadow: 0px 4px 4px -4px rgba(79, 81, 89, 0.32),
-        0px 2px 5px -2px rgba(79, 81, 89, 0.03),
-        0px 0px 0px 1px rgba(188, 189, 194, 0.25),
-        0px 1px 1px 0px rgba(188, 189, 194, 0.20);
-    `}
+        display: flex;
+      `}
     >
-      <button
-        className={css`
-        box-sizing: border-box;
-        display: flex;
-        padding: 6px 12px;
-        justify-content: center;
-        align-items: center;
-        gap: 10px;
-        flex: 1 0 0;
-        border: none;
-        border-color: ${basis.border};
-        background: ${isClicked ? white : basis.bg_subtle};
-        color: ${isClicked ? basis.text : primary.text};
-        text-align: center;
-        font-size: 12px;
-        font-family: PingFang SC;
-        font-weight: 500;
-        line-height: 16px;
-      `}
-        onClick={handleClick}
-      >
-        常用应用
-      </button>
-      <div
-        className={css`
-          width: 1px;
-          height: 28px;
-          position: relative;
-          left: -1px;
-          background-color: ${basis.border};
-          margin: 0;
-        `}
-      ></div>
-      <button
-        className={css`
-        box-sizing: border-box;
-        display: flex;
-        padding: 6px 12px;
-        justify-content: center;
-        align-items: center;
-        gap: 10px;
-        flex: 1 0 0;
-        border: none;
-        border-color: ${basis.border};
-        background: ${isClicked ? basis.bg_subtle : white};
-        color: ${isClicked ? primary.text : basis.text};
-        text-align: center;
-        font-size: 12px;
-        font-family: PingFang SC;
-        font-weight: 500;
-        line-height: 16px;
-      `}
-        onClick={handleClick}
-      >
-        使用记录
-      </button>
+      {React.Children.map(props.children, (child, index) => (
+        <div
+          className={css`
+            width: 104px;
+            height: 28px;
+            border: 1px solid;
+            overflow: hidden;
+            border-color: ${basis.border};
+            border-top-left-radius: ${index === 0 ? '6px' : '0px'};
+            border-bottom-left-radius: ${index === 0 ? '6px' : '0px'};
+            border-top-right-radius: ${index === childrenCount - 1 ? '6px' : '0'};
+            border-bottom-right-radius: ${index === childrenCount - 1 ? '6px' : '0'};
+            margin-right: ${index >= 0 && childrenCount > 2 ? '-1px' : '0'};
+            background: ${index === activeButton
+              ? basis.bg_subtle // Set the background color for the active button
+              : index === prevActiveButton
+                ? fill.base.layer_chrome // Set the background color for the previously active button
+                : "transparent" // Set the default background color for other buttons
+            };
+            color: ${index === activeButton
+              ? primary.text
+              : index === prevActiveButton
+                ? basis.text
+                : "transparent"
+            };
+          `}
+          onClick={() => handleButtonClick(index)}
+        >
+          {React.cloneElement(child as React.ReactElement, {
+            variant: "selected",
+          })}
+        </div>
+      ))}
     </div>
   );
-};
+}
 
 export default ButtonGroup;
