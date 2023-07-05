92aeaeb4b013e4bf2d6a5b48fe2c6d262c7363f0
yutinggh <134508301+yutinggh@users.noreply.github.com>
2023-07-05 14:01:26 +0800

    Update ButtonGroup.tsx

--- src/components/shared/ButtonGroup.tsx
+++ src/components/shared/ButtonGroup.tsx
@@ -1,59 +1,36 @@
 import { css } from "@emotion/css";
-import React, { useState } from "react";
-import { basis, fill, primary } from "components/constants/colors";
+import { basis } from "components/constants/colors";
+import { ReactNode } from "react";
+import { ButtonGroupCell } from "./ButtonGroupCell";
 
-interface ButtonGroupProps {
-  children: React.ReactNode;
-}
-function ButtonGroup(props: ButtonGroupProps) {
-  const [activeButton, setActiveButton] = useState<number | null>(null);
-  const [prevActiveButton, setPrevActiveButton] = useState<number | null>(null);
-  const handleButtonClick = (index: number) => {
-    setPrevActiveButton(activeButton);
-    setActiveButton(index);
-  };
-  const childrenCount = React.Children.count(props.children);
+const ButtonGroup = ({ children }: { children: ReactNode }) => {
   return (
     <div
       className={css`
         display: flex;
+        box-shadow: 0px 1px 1px rgba(188, 189, 194, 0.2),
+          0px 0px 0px 1px rgba(188, 189, 194, 0.25),
+          0px 2px 5px -2px rgba(79, 81, 89, 0.03),
+          0px 4px 4px -4px rgba(79, 81, 89, 0.32);
+        border-radius: 6px;
+        & > :first-child {
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+          border-right: 1px solid ${basis.border};
+        }
+        & > :last-child {
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+        & > :not(:first-child):not(:last-child) {
+          border-radius: 0;
+          border-right: 1px solid ${basis.border};
+        }
       `}
     >
-      {React.Children.map(props.children, (child, index) => (
-        <div
-          className={css`
-            width: 104px;
-            height: 28px;
-            border: 1px solid;
-            overflow: hidden;
-            border-color: ${basis.border};
-            border-top-left-radius: ${index === 0 ? '6px' : '0px'};
-            border-bottom-left-radius: ${index === 0 ? '6px' : '0px'};
-            border-top-right-radius: ${index === childrenCount - 1 ? '6px' : '0'};
-            border-bottom-right-radius: ${index === childrenCount - 1 ? '6px' : '0'};
-            margin-right: ${index >= 0 && childrenCount > 2 ? '-1px' : '0'};
-            background: ${index === activeButton
-              ? basis.bg_subtle // Set the background color for the active button
-              : index === prevActiveButton
-                ? fill.base.layer_chrome // Set the background color for the previously active button
-                : "transparent" // Set the default background color for other buttons
-            };
-            color: ${index === activeButton
-              ? primary.text
-              : index === prevActiveButton
-                ? basis.text
-                : "transparent"
-            };
-          `}
-          onClick={() => handleButtonClick(index)}
-        >
-          {React.cloneElement(child as React.ReactElement, {
-            variant: "selected",
-          })}
-        </div>
-      ))}
+      {children}
     </div>
   );
-}
+};
 
-export default ButtonGroup;
+export { ButtonGroup, ButtonGroupCell };
