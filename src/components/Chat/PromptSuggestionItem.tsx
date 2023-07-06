bd0dbae40f1893342c091b1b1fb1800be5849310
yutinggh <134508301+yutinggh@users.noreply.github.com>
2023-07-06 17:42:20 +0800

    feat: prompt-suggestion-item

---
+++ src/components/Chat/PromptSuggestionItem.tsx
@@ -0,0 +1,94 @@
+import { css } from '@emotion/css';
+import { basis, rainbow } from "components/constants/colors";
+
+const PromptSuggestionItem = () => (
+  <div
+    className={css`
+      display: flex;
+      padding: 20px;
+      align-items: center;
+      gap: 16px;
+      flex: 1 0 0;
+      align-self: stretch;
+      width: 222px;
+      height: 68px;
+    `}
+  >
+    <div
+      className={css`
+        flex-direction: column;
+      `}
+    >
+      <div
+        className={css`
+        display: flex;
+        align-items: center;
+      `}
+      >
+        <p
+          className={css`
+          display: flex;
+          flex-direction: column;
+          flex: 1 0 0;
+          color: ${basis.text_loud};
+          font-family: PingFang SC;
+          font-size: 14px;
+          font-style: normal;
+          font-weight: 500;
+          line-height: 20px;
+          margin-right: 36px;
+          margin-bottom: 16px;
+        `}
+        >
+          品牌策划文案
+        </p>
+        <div
+          className={css`
+          display: flex;
+          padding: 0px 6px;
+          justify-content: center;
+          align-items: center;
+          gap: 8px;
+          border-radius: 108px;
+          border: 1px solid ${rainbow.blue_alt.border};
+          background: ${rainbow.blue_alt.bg_muted};
+          width: 52px;
+          height: 20px;
+        `}
+        >
+          <div
+            className={css`
+            color: ${rainbow.blue.text_loud};
+            text-align: center;
+            font-family: PingFang SC;
+            font-size: 12px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: 16px;
+          `}
+          >
+            创作模式
+          </div>
+        </div>
+      </div>
+      <p
+        className={css`
+        display: flex;
+        flex-direction: column;
+        align-self: stretch;
+        color: ${basis.text};
+        font-family: PingFang SC;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 16px;
+        margin-top: 0px;
+      `}
+      >
+        快速生成文案。快速生成文案快速生成文案。快速生成文案。快速生成...
+      </p>
+    </div>
+  </div>
+);
+
+export { PromptSuggestionItem };
