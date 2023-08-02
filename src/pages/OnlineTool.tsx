3360edc91a989a94e2ccd31b0d5ce39f298a7466
Dylan Tse <dylan052099@gmail.com>
2023-08-01 20:24:33 -0700

    feat: isolate online tool page body

--- src/pages/OnlineTool.tsx
+++ src/pages/OnlineTool.tsx
@@ -1,6 +1,5 @@
 import { css } from "@emotion/css";
-import { ChooseModelDialog } from "components/OnlineTool/ChooseModelDialog";
-import { KnowledgeBase } from "components/OnlineTool/KnowledgeBase";
+import { OnlineToolBody } from "components/OnlineTool/OnlineToolBody";
 import { OnlineToolHeader } from "components/OnlineTool/OnlineToolHeader";
 import { OnlineToolSideBar } from "components/OnlineTool/OnlineToolSideBar";
 import { PrimaryNavBar } from "components/PrimaryNavBar";
@@ -152,73 +151,7 @@ const OnlineTool = () => {
             selectedItem={selectedItem}
             handleSelectedItemChange={handleSelectedItemChange}
           />
-          {mode === ToolsMode.Home ? (
-            <>home</>
-          ) : mode === ToolsMode.Chat ? (
-            <div
-              className={css`
-                display: flex;
-                flex-direction: column;
-                flex: 1;
-                border-radius: 8px;
-                border: 1px solid ${basis.border};
-                margin: 12px;
-              `}
-            >
-              <div
-                className={css`
-                  display: flex;
-                  flex-direction: column;
-                  flex: 1;
-                  & :first-child {
-                    border-top-right-radius: 8px;
-                    border-top-left-radius: 8px;
-                  }
-                `}
-              >
-                {/* <UserChatItem>
-                  I want you to act as a UX/UI developer. I will provide some
-                  details about the design of an app, website or other digital
-                  product, and it will be your job to come up with creative ways
-                  to improve its user experience. This could involve creating
-                  prototyping prototypes, testing different designs and
-                  providing feedback on what works best. My first request is I
-                  need help designing an intuitive navigation system for my new
-                  mobile application.
-                </UserChatItem>
-                <SystemChatItem
-                  prepend={
-                    <img
-                      className={css`
-                        width: 28px;
-                        height: 28px;
-                      `}
-                      src={SystemAvatar}
-                      alt="System avatar"
-                    />
-                  }
-                >
-                  As a pet behaviorist, I'm here to help you address the
-                  aggression issues with your German Shepherd. Aggression in
-                  dogs can have various underlying causes, and it's important to
-                  understand the root cause before implementing a behavior
-                  modification plan. Here are the steps we can take to help
-                  manage your dog's aggressions.
-                </SystemChatItem>
-                <DialogHeader /> */}
-                {isChooseModelDialogVisible && <ChooseModelDialog />}
-              </div>
-              {/* <ChatInput /> */}
-            </div>
-          ) : mode === ToolsMode.Compose ? (
-            <>compose</>
-          ) : mode === ToolsMode.Paint ? (
-            <>paint</>
-          ) : mode === ToolsMode.Podcast ? (
-            <>podcast</>
-          ) : (
-            <KnowledgeBase />
-          )}
+          <OnlineToolBody mode={mode} />
         </div>
       </div>
     </div>
